96296b2920503c71381b21a23c17f5877e15adec
ildenio <ildeniocosta@gmail.com>
2019-07-17 10:20:57 -0300

    implementando toString de pedido

--- src/main/java/com/ildenio/curso/services/PedidoService.java
+++ src/main/java/com/ildenio/curso/services/PedidoService.java
@@ -10,6 +10,7 @@ import com.ildenio.curso.repositories.PedidoRepository;
 import com.ildenio.curso.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.Optional;
@@ -26,15 +27,20 @@ public class PedidoService {
     private ProdutoService produtoService;
     @Autowired
     private ItemPedidoRepository itemPedidoRepository;
+    @Autowired
+    private ClienteService clienteService;
 
     public Pedido find(Integer id) {
         Optional<Pedido> obj = repo.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! id: "+id+" ,Tipo: "+Pedido.class.getName()));
     }
+
+    @Transactional
     public Pedido insert(Pedido obj){
 
         obj.setId(null);
         obj.setInstante(new Date());
+        obj.setCliente(clienteService.find(obj.getCliente().getId()));
         obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
         obj.getPagamento().setPedido(obj);
         if(obj.getPagamento() instanceof PagamentoComBoleto){
@@ -45,10 +51,12 @@ public class PedidoService {
         pagamentoRepository.save(obj.getPagamento());
         for(ItemPedido ip : obj.getItens()){
             ip.setDesconto(0.0);
-            ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+            ip.setProduto(produtoService.find(ip.getProduto().getId()));
+            ip.setPreco(ip.getProduto().getPreco());
             ip.setPedido(obj);
         }
         itemPedidoRepository.saveAll(obj.getItens());
+        System.out.println(obj);
           return obj;
     }
 
