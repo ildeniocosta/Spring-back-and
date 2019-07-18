ac95a327397866d47a1566583182a8c8fa19fad9
ildenio <ildeniocosta@gmail.com>
2019-07-18 11:20:30 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/ildenio/curso/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ildenio.curso.services;
+
+import com.ildenio.curso.domain.Cliente;
+import com.ildenio.curso.repositories.ClienteRepository;
+import com.ildenio.curso.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
