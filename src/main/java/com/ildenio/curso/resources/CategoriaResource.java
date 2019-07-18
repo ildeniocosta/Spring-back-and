35d58dcd948b8b60fd47d834e74ffa1602adf5d0
ildenio <ildeniocosta@gmail.com>
2019-07-18 12:47:49 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/ildenio/curso/resources/CategoriaResource.java
+++ src/main/java/com/ildenio/curso/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import com.ildenio.curso.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,11 +23,14 @@ public class CategoriaResource {
 
     @Autowired
     private CategoriaService service;
+
+
     @RequestMapping(value = "/{id}",method= RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id){
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
         Categoria obj =service.fromDto(objDto);
@@ -34,6 +38,7 @@ public class CategoriaResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto,@PathVariable Integer id){
         Categoria obj = service.fromDto(objDto);
@@ -41,11 +46,13 @@ public class CategoriaResource {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}",method= RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id){
       service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<CategoriaDTO>>findAll() {
         List<Categoria> list = service.findAll();
--- src/main/java/com/ildenio/curso/resources/ClienteResource.java
+++ src/main/java/com/ildenio/curso/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import com.ildenio.curso.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,6 +30,7 @@ public class ClienteResource {
         return ResponseEntity.ok().body(obj);
 
     }
+
     @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
         Cliente obj = service.fromDto(objDto);
@@ -36,11 +38,13 @@ public class ClienteResource {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}",method= RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<ClienteDTO>>findAll() {
         List<Cliente> list = service.findAll();
@@ -54,6 +58,7 @@ public class ClienteResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value="/page", method=RequestMethod.GET)
     public ResponseEntity<Page<ClienteDTO>> findPage(
             @RequestParam(value="page", defaultValue="0") Integer page,
