dd2d0cd7f85390820bb407a8d1e8be5d7c40effc
ildenio <ildeniocosta@gmail.com>
2019-07-18 22:42:25 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/ildenio/curso/CursoApplication.java
+++ src/main/java/com/ildenio/curso/CursoApplication.java
@@ -1,11 +1,15 @@
 package com.ildenio.curso;
 
+import com.ildenio.curso.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner {
+    @Autowired
+    private S3Service s3Service;
 
 
     public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class CursoApplication implements CommandLineRunner {
     }
     @Override
     public void run(String... args)throws Exception{
-
+        s3Service.uploadFile("C:\\Users\\Predator\\Pictures\\Saved Pictures\\pena.jpg");
 
 
 
